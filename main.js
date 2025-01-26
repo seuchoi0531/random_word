e7312be8fdd8d9f2b946c3667eeabf4a1a3e94a0
seuchoi0531 <seuchoi0531@gmail.com>
2025-01-26 19:43:40 +0900

    switch 2 column to 4 column, add toggledrag, add toPDF, fix error about last word index, will update the function collect and review unknown words

--- main.js
+++ main.js
@@ -5,15 +5,22 @@ $(document).ready(function () {
     let seeonlymeaning = document.getElementById("see_only_meaning");
     let seeboth = document.getElementById("see_both");
     let mix = document.getElementById("mix");
+    let isDrag = document.getElementById("isDrag");
+    let body = document.getElementsByTagName("body")[0];
+    let toPDF = document.getElementById("toPDF");
+    let isDragable = false;
     let words = [];
+    let bookmarks = [];
     inputfile.addEventListener('change', (e) => {
         const file = e.target.files[0];
         generateWord(file);
     })
-    seeonlyword.addEventListener('click',showOnlyWord);
-    seeonlymeaning.addEventListener('click',showOnlyMeaning);
-    seeboth.addEventListener('click',showBothWordAndMeaning);
-    mix.addEventListener('click',mixWords);
+    seeonlyword.addEventListener('click', showOnlyWord);
+    seeonlymeaning.addEventListener('click', showOnlyMeaning);
+    seeboth.addEventListener('click', showBothWordAndMeaning);
+    mix.addEventListener('click', mixWords);
+    isDrag.addEventListener('click', toggleDrag);
+    toPDF.addEventListener('click', saveToPDF);
     function generateWord(file) {
         let reader = new FileReader();
         reader.onload = function (event) {
@@ -30,65 +37,98 @@ $(document).ready(function () {
                     str = '';
                     words.push(tmpob);
                     tmpob = {};
-                } else {
+                } else
                     str += i;
-                }
             }
             makeTable();
-
             console.log(words);
         };
-
         reader.readAsText(file);
     }
 
     function makeTable() {
         console.log("table");
         let result = '<table>';
-        for (let i = 0; i < words.length; i++) {
-            if (i % 10 == 0)
-                result += `<tr class="boldborder"><td>${words[i].word}</td><td>${words[i].meaning}</td></tr>`;
-            else if (i % 2 == 1)
-                result += `<tr class="oddtr"><td>${words[i].word}</td><td>${words[i].meaning}</td></tr>`;
-            else
-                result += `<tr><td>${words[i].word}</td><td>${words[i].meaning}</td></tr>`;
+        for (let i = 0; i < words.length; i += 2) {
+            if (i + 1 < words.length) {
+                if (i != 0 && i % 20 == 0)
+                    result += `<tr class="boldborder"><td>${words[i].word}</td><td>${words[i].meaning}</td><td>${words[i + 1].word}</td><td>${words[i + 1].meaning}</td></tr>`;
+                else if ((i / 2) % 2 == 1)
+                    result += `<tr class="oddtr"><td>${words[i].word}</td><td>${words[i].meaning}</td><td>${words[i + 1].word}</td><td>${words[i + 1].meaning}</td></tr>`;
+                else
+                    result += `<tr><td>${words[i].word}</td><td>${words[i].meaning}</td><td>${words[i + 1].word}</td><td>${words[i + 1].meaning}</td></tr>`;
+            }
+            else {
+                if (i != 0 && i % 20 == 0)
+                    result += `<tr class="boldborder"><td>${words[i].word}</td><td>${words[i].meaning}</td><td></td><td></td></tr>`;
+                else if ((i / 2) % 2 == 1)
+                    result += `<tr class="oddtr"><td>${words[i].word}</td><td>${words[i].meaning}</td><td></td><td></td></tr>`;
+                else
+                    result += `<tr><td>${words[i].word}</td><td>${words[i].meaning}</td><td></td><td></td></tr>`;
+            }
         }
         result += '</table>';
         table.innerHTML = result;
+        const tds = document.getElementsByTagName("td");
+        Array.from(tds).forEach((td, i) => {
+            td.addEventListener('click', () => {
+                if (td.innerText == '') {
+                    if (i % 2 == 0 && words.length > i / 2)
+                        td.innerText = words[i / 2].word;
+                    else if (i % 2 == 1 && words.length > (i - 1) / 2)
+                        td.innerText = words[(i - 1) / 2].meaning;
+                } else
+                    td.innerText = '';
+            })
+        })
     }
     function showOnlyWord() {
         const tds = document.getElementsByTagName("td");
         for (let i = 0; i < tds.length; i++) {
-            if (i % 2 == 0) {
-                tds[i].innerText = words[i / 2].word;
-            } else {
-                tds[i].innerText = '';
+            if (words.length > i / 2) {
+                if (i % 2 == 0)
+                    tds[i].innerText = words[i / 2].word;
+                else
+                    tds[i].innerText = '';
             }
         }
     }
     function showOnlyMeaning() {
         const tds = document.getElementsByTagName("td");
         for (let i = 0; i < tds.length; i++) {
-            if (i % 2 == 1) {
-                tds[i].innerText = words[(i - 1) / 2].meaning;
-            } else {
-                tds[i].innerText = '';
+            if (words.length > i / 2) {
+                if (i % 2 == 1)
+                    tds[i].innerText = words[(i - 1) / 2].meaning;
+                else
+                    tds[i].innerText = '';
             }
         }
     }
-    function showBothWordAndMeaning(){
+    function showBothWordAndMeaning() {
         const tds = document.getElementsByTagName("td");
         for (let i = 0; i < tds.length; i++) {
-            if (i % 2 == 0) {
-                tds[i].innerText = words[i / 2].word;
-            } else {
-                tds[i].innerText = words[(i - 1) / 2].meaning;
+            if (words.length > i / 2) {
+                if (i % 2 == 0)
+                    tds[i].innerText = words[i / 2].word;
+                else
+                    tds[i].innerText = words[(i - 1) / 2].meaning;
             }
         }
     }
-    function mixWords(){
+    function mixWords() {
         words.sort(() => Math.random() - 0.5)
         console.log(words);
         makeTable();
+    }
+    function toggleDrag() {
+        if (isDragable)
+            body.setAttribute("onselectstart", "return false");
+        else
+            body.setAttribute("onselectstart", "return true");
+        isDragable = !isDragable;
+        console.log(isDragable);
+    }
+    function saveToPDF() {
+        html2pdf(table);
     }
 });
